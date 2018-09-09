0cb1b3b28d8f1d89dd575c927fd9607de11bd756
shibithangam <35791250+shibithangam@users.noreply.github.com>
2018-09-08 23:12:39 -0600

    Add files via upload

---
+++ scripts/diskrep.py
@@ -0,0 +1,22 @@
+'''
+Created on Sep 8, 2018
+
+@author: shibi
+'''
+import smtplib
+import subprocess
+import string
+from sys import stdout
+p=subprocess.Popen("df -h", shell=True, stdout=subprocess.PIPE)
+Msg=p.stdout.read()
+From="shibinavin@gmail.com"
+To="shibinavin@gmail.com"
+Subject="Nightly disk usage report"
+msg=string.join(("From:%s" % From,
+                 "To: %s" % To,
+                 "Subject: %s" % Subject,
+                 "",
+                 Msg), "\r\n")
+server=smtplib.SMTP('localhost')
+server.sendmail(From,To,msg)
+server.quit()
